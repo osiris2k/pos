f89c4c3b023910d49b90181c61f0a00ccb994653
Boripat <boripat.b@quo-global.com>
2018-03-23 17:59:27 +0700

    Create main page / sale index

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import Sale from './sale/home';
 
-import './back-office/theme/css/bootstrap.min.css';
-import './back-office/theme/css/font-awesome.min.css';
-import './back-office/theme/css/AdminLTE.css';
+import './theme/css/bootstrap.min.css';
+import './theme/css/font-awesome.min.css';
+import './theme/css/AdminLTE.css';
 
 class App extends Component {
 	render() {
--- src/sale/home.js
+++ src/sale/home.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
+
 
 class Sale extends Component{
 
 
 	render(){
 		return(
-			<p>
-				Ready!
-			</p>
+			<div className="wrapper row-offcanvas row-offcanvas-left">
+				<aside className="right-side strech">
+					<section className="content-header">
+						<h1>
+							POS <i className="fa fa-warning text-yellow"></i> Oops! Something went wrong.
+						</h1>
+						<ol className="breadcrumb">
+							<li><Link to="/login"><i className="fa fa-key"></i> Login</Link></li>
+							<li className="active">Log off</li>
+						</ol>
+					</section>
+
+					<section className="content">
+
+					</section>
+				</aside>
+			</div>
 		)
 	}
 }
